9cbde14a3ddcabf3a26f2c4b33594906c325d2df
Daniel Lenksjo <Daniel Lenksjo>
2016-01-26 19:43:15 +0100

    Added .net solution

---
+++ ws-dotnet/src/Cygni.Competence.Reactive/Handler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reactive.Subjects;
+
+namespace Cygni.Competence.Reactive
+{
+    public class Handler
+    {
+        public Handler(IObserver<object> messages)
+        {
+            Messages = messages;
+            Messages.OnNext(new { links = new[] { "http://hej" } });
+            QueryInputs = new Subject<string>();
+            GoClicks = new Subject<string>();
+            InstantSearchChanges = new Subject<bool>();
+            EnterPresses = new Subject<string>();
+        }
+
+        public Subject<string> EnterPresses { get; }
+
+        public Subject<bool> InstantSearchChanges { get; }
+
+        public IObserver<object> Messages { get; }
+
+        public Subject<string> GoClicks { get; }
+
+        public Subject<string> QueryInputs { get; }
+    }
+}
