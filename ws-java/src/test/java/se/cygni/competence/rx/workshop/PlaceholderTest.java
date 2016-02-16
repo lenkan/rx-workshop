7386b23d62a7a06890d210d0fb5dbfd6eb0cc5d6
Alexander Torstling <alexander@torstling.com>
2016-02-16 20:11:14 +0100

    remove facit

---
+++ ws-java/src/test/java/se/cygni/competence/rx/workshop/PlaceholderTest.java
@@ -0,0 +1,15 @@
+package se.cygni.competence.rx.workshop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by alext on 2016-02-16.
+ */
+public class PlaceholderTest {
+
+    @Test
+    public void one() {
+        Assert.assertEquals(3,1+2);
+    }
+}
