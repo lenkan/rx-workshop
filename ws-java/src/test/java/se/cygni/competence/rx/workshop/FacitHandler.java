6e8c73218a31327de4c43c87bfcac36c9c72863d
Alexander Torstling <alexander@torstling.com>
2016-02-18 23:04:18 +0100

    clickable links, interleave results

--- ws-java/src/test/java/se/cygni/competence/rx/workshop/FacitHandler.java
+++ ws-java/src/test/java/se/cygni/competence/rx/workshop/FacitHandler.java
@@ -6,6 +6,8 @@ import rx.observables.ConnectableObservable;
 
 import java.net.URI;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -30,7 +32,7 @@ public class FacitHandler implements ConnectionHandler {
         Observable<String> textOnGoClick = queryInputs.sample(goClicks);
         final Observable<String> textOnEnterPress = queryInputs.sample(enterPresses);
         final Observable<String> textOnTypeWhenInstantEnabled = Observable.combineLatest(queryInputs,
-                instantSearchChanges, (phrase, checked) -> checked? phrase : "").filter(p -> !p.isEmpty());
+                instantSearchChanges, (phrase, checked) -> checked ? phrase : "").filter(p -> !p.isEmpty());
         textOnTypeWhenInstantEnabled.map(o -> "listening").subscribe(status);
         Observable<String> debouncedTextOnTypeWhenInstantEnabled = textOnTypeWhenInstantEnabled.debounce(1, TimeUnit.SECONDS);
         Observable<String> textOnAction = textOnGoClick.mergeWith(textOnEnterPress).mergeWith(debouncedTextOnTypeWhenInstantEnabled);
@@ -39,9 +41,9 @@ public class FacitHandler implements ConnectionHandler {
             System.out.println("About to search");
         });
         final Observable<List<String>> requests = textOnAction.flatMap((searchTerm) -> {
-            Observable<List<String>> dResults = duckDuckGoClient.searchRelated(searchTerm);
-            Observable<List<String>> wResults = new WikipediaSearchClient().searchRelated(searchTerm);
-            return dResults.mergeWith(wResults);
+            Observable<List<String>> dResultLists = duckDuckGoClient.searchRelated(searchTerm);
+            Observable<List<String>> wResultLists = new WikipediaSearchClient().searchRelated(searchTerm);
+            return dResultLists.zipWith(wResultLists, this::interleave);
         });
         //Don't want two subscribers on requests since it triggers double requests being issued
         final ConnectableObservable<List<String>> doneRequests = requests.publish();
@@ -49,6 +51,19 @@ public class FacitHandler implements ConnectionHandler {
         doneRequests.subscribe(links);
         doneRequests.connect();
         status.onNext("ready");
+    }
+
+    private <T> ArrayList<T> interleave(List<? extends T> as, List<? extends T> bs) {
+        final ArrayList<T> rs = new ArrayList<>();
+        for(Iterator<? extends T> ia = as.iterator(), ib = bs.iterator(); ia.hasNext() || ib.hasNext();) {
+            if (ia.hasNext()) {
+                rs.add(ia.next());
+            }
+            if (ib.hasNext()) {
+                rs.add(ib.next());
+            }
+        }
+        return rs;
     }
 
     public static void main(String[] args) throws InterruptedException, UnknownHostException {
